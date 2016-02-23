156da65fbe32fbdbd3573e07e6fce857e77ae3dd
Patrick Yeon <patrickyeon@gmail.com>
2016-02-23 02:00:07 -0800

    ok, now the real MVP is done

--- bg.js
+++ bg.js
@@ -1,10 +1,16 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request.action == "showIcon") {
+      if (request.action == "earlyIcon") {
         chrome.pageAction.show(sender.tab.id);
         chrome.pageAction.setTitle({tabId: sender.tab.id,
                                     title: ("urls: " + request.urls +
                                             " locs: " + request.locs)});
+      } else if (request.action == "lateIcon") {
+        chrome.pageAction.show(sender.tab.id);
+        chrome.pageAction.setTitle({tabId: sender.tab.id,
+                                    title: ("urls: " + request.urls + 
+                                            " locs: " + request.locs
+                                            " scenes: " + request.scenes)});
       }
     });
 
--- locscrape.js
+++ locscrape.js
@@ -1,7 +1,8 @@
 var latlonUrl = /https:\/\/tools.wmflabs.org\/\S*params=([\d_NSEW\.]+)/;
 var angleFrac = /(\d+)_(\d+)_(\d+)_([NS])_(\d+)_(\d+)_(\d+)_([EW])/;
 var angleDec = /(\d+\.\d+)_([NS])_(\d+\.\d+)_([EW])/;
-var scenesUrl = "https://www.planet.com/scenes/#/zoom/13/acquired/0/geometry/"
+var scenesUrl = "https://www.planet.com/scenes/#/zoom/13/acquired/0/geometry/";
+var apiUrl = "https://api.planet.com/v0/scenes/ortho/?intersects=";
 // TODO locations can also be nn_nn_N_nn_nn_W and nn_n.nn_N_nn_n.nn_W
 //       so I need a better parser
 
@@ -44,28 +45,62 @@ function parseDecLoc(str) {
   return [lat, lon];
 }
 
+function scenesLink(lat, lon, elt, scenecount) {
+  var ln = document.createElement('a');
+  ln.setAttribute('href', scenesUrl + "POINT(" + lon + "%20" + lat + ")"
+                          + "/center/" + lon + "," + lat);
+  ln.appendChild(document.createTextNode("(" + scenecount + " scenes)"));
+  var par = elt.parentElement;
+  if (elt.nextSibling === null) {
+    par.appendChild(ln);
+  } else {
+    par.insertBefore(ln, elt.nextSibling);
+  }
+  par.insertBefore(document.createTextNode("  "), ln);
+}
+
+function sceneify(ln, i) {
+  if (i >= ln.length) {
+    return 0;
+  }
+  var matches = latlonUrl.exec(ln[i]);
+  if (matches === null) {
+    return sceneify(ln, i + 1);
+  }
+  var loc = parseLoc(matches[1]);
+
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+      var sceneDat = JSON.parse(req.responseText);
+      scenesLink(loc[0], loc[1], ln[i], sceneDat.count);
+      return(sceneify(ln, i + 1) + sceneDat.count);
+    }
+  };
+  req.open("GET", apiUrl + "POINT(" + loc[1] + "%20" + loc[0] + ")", true);
+           //true, authkey, authkey);
+  req.setRequestHeader("Authorization", "Basic " + btoa(authkey + ":" + authkey));
+  req.send(null);
+}
+
 var links = document.getElementsByTagName("a");
 
 var locCount = 0;
 for (i = 0; i < links.length; i++) {
   var matches = latlonUrl.exec(links[i]);
   if (matches !== null) {
-    var loc = parseLoc(matches[1]);
-    var ln = document.createElement('a');
-    ln.setAttribute('href', scenesUrl + "POINT(" + loc[1] + "%20" + loc[0] + ")"
-                            + "/center/" + loc[1] + "," + loc[0]);
-    ln.appendChild(document.createTextNode("(scenes)"));
-    var par = links[i].parentElement;
-    if (links[i].nextSibling === null) {
-      par.appendChild(ln);
-    } else {
-      par.insertBefore(ln, links[i].nextSibling);
-    }
-    par.insertBefore(document.createTextNode("  "), ln);
     locCount++;
   }
 }
 
-chrome.runtime.sendMessage({action: "showIcon",
+chrome.runtime.sendMessage({action: "earlyIcon",
                             urls: links.length,
                             locs: locCount}, function(response) {});
+
+var sceneCount = sceneify(links, 0);
+
+// currently doesn't work, not actually returning sceneCount properly
+//chrome.runtime.sendMessage({action: "lateIcon",
+//                            urls: links.length,
+//                            locs: locCount,
+//                            scenes: sceneCount}, function(response) {});
