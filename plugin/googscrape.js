8ee8db108b2c4e0adff6279a6e3163e0cb818d40
Patrick Yeon <patrickyeon@gmail.com>
2016-03-12 12:36:13 -0800

    fix race condition while navigating google searches

--- plugin/googscrape.js
+++ plugin/googscrape.js
@@ -25,13 +25,25 @@ function mkLink(observer) {
           || mapImg === null) {
         return;
       }
+      // There is a race condition where we've kicked off mkScenesLink() and the
+      //  page has changed (eg. due to the user's typing) before it has finished
+      //  the scenes query and called the callback. In that case, we could add
+      //  an incorrect scenes link. So we check to make sure the current infobox
+      //  map is for the same location as the callback is for.
+      var newPos = latlon.exec(mapImg.parentElement.href);
+      if (newPos[1] != pos[1] || newPos[2] != pos[2]) {
+        return;
+      }
+
       // find the <ol> element that is the infobox
       var elt = mapImg;
       while (elt !== null && elt.tagName.toLowerCase() != "ol") {
         elt = elt.parentElement;
       }
       if (elt === null) {
-        alert("couldn't back out to the infobox");
+        // couldn't back out to the infobox. I actually haven't seen this
+        //  happen, but I'm not 100% sure that it never will.
+        return;
       }
       var modElts = elt.getElementsByClassName("mod");
       // the third mod elt is the name in large text in the box
@@ -41,12 +53,9 @@ function mkLink(observer) {
       scenediv.appendChild(lnk);
       modElts[3].parentElement.insertBefore(scenediv, modElts[3]);
     });
-    observer.observe(document, {"childList": true, "attributes": true,
-                                "characterData": true, "subtree": true});
-  } else {
-    observer.observe(document, {"childList": true, "attributes": true,
-                                "characterData": true, "subtree": true});
   }
+  observer.observe(document, {"childList": true, "attributes": true,
+                              "characterData": true, "subtree": true});
 }
 
 mkLink(watcher);
