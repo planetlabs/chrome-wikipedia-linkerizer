8dc4a436057a9de1b82b2142e4f0b48ceeeb36c1
Patrick Yeon <patrickyeon@gmail.com>
2016-05-20 11:25:32 -0700

    licensing

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,3 +1,20 @@
+#
+# Purpose: Shim to get scene count from the API without shipping an API key
+#
+# Copyright 2016, Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from time import time
 import json, requests
 
--- plugin/scenerize.js
+++ plugin/scenerize.js
@@ -1,3 +1,21 @@
+/**
+ * Purpose: Fetch the scene count and create a link
+ *
+ * Copyright 2016, Planet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var scenesUrl = "https://www.planet.com/scenes/#/zoom/12/acquired/0/geometry/";
 var apiUrl = "https://9ega98lmsd.execute-api.us-west-2.amazonaws.com/prod?"
 
